86156c6f16884623e9c7900270470d59805903a0
arnoudHauw <fiets_arnoud@live.nl>
2020-01-12 03:50:21 +0100

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller; 
+
+    public float movementSpeed = 5.0f;
+    public float rotationSpeed = 200.0f;
+
+    void start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+    }
+}
